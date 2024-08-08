c562a61a57615227a47960288083f6904950cfa7
Wilfried Mercier <wilfried.mercier58@gmail.com>
2024-08-08 15:46:33 +0200

    collection of miscellaneous classes used in the code

---
+++ misc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+r"""
+..codeauthor:: Mercier Wilfried - LAM <wilfried.mercier@lam.fr>
+
+Miscellaneous classes.
+"""
+
+import numpy as np
+
+class ArrayList(list):
+    r'''
+    .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
+    
+    Custom list that only allows numpy.ndarray elements of dimensions 2 or 3.
+    '''
+    
+    def __init__(self, *args, **kwargs) -> None:
+        
+        super().__init__(*args, **kwargs)
+        return
+    
+    def append(self, array: np.ndarray) -> None:
+        r'''    
+        .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
+        
+        Custom append method that checks that the given element is a numpy.ndarray of dimensions 2 or 3.
+        
+        :param array: 2D or 3D array
+        :type array: numpy.ndarray
+        
+        :raises:
+            * :python:`TypeError` if `not isinstance(array, np.ndarray)`
+            * :python:`ValueError` if `array.ndim < 2 or array.ndim > 3`
+        '''
+        
+        if not isinstance(array, np.ndarray):
+            raise TypeError(f'array to append has type {type(array)} but it must have type numpy.ndarray')
+
+        if array.ndim < 2 or array.ndim > 3:
+            raise ValueError(f'array has dimensions {array.ndim} but only data of dimension 2 (images) or 3 (cubes) are allowed.')
+
+        super().append(array)
+        return
