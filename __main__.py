b3ba60a96a8aa418bc55a9cf15d54eb82e755364
Wilfried Mercier <wilfried.mercier58@gmail.com>
2024-08-09 09:23:38 +0200

    moved some of the classes to other files

--- __main__.py
+++ __main__.py
@@ -9,64 +9,79 @@ Inspired by PyQubeVis (https://gitlab.lam.fr/bepinat/PyQubeVis/-/tree/master?ref
 
 import sys
 import signal
-import numpy                              as np
-import matplotlib.pyplot                  as plt
-import matplotlib.figure                  as     mplf
-from   PyQt6.QtWidgets                    import QApplication, QMainWindow, QWidget, QVBoxLayout
-from   matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import argparse
+import os.path                            as     opath
+import numpy                              as     np
+from   astropy.io                         import fits
+from   PyQt6.QtWidgets                    import QApplication, QMainWindow
+from   PyQt6.QtCore                       import Qt
 
-class Mpl_im_canvas(FigureCanvas):
-    r'''
-    .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
+# Custom imports
+from   mpl_custom                        import Mpl_im_canvas, Dock_widget_spectrum
+
+class Window(QMainWindow):
     
-    Custom matplotlib canvas that can hold an image.
-    
-    Heavily inspired by the mplCanvas class in PyQubeVis.
-    '''
-    
-    # Class attribute is the list of allowed cmaps from matplotlib
-    __cmaps_ok = plt.colormaps()
-    
-    def __init__(self, parent: QWidget, cmap: str) -> None:
-        r'''    
+    def __init__(self, args: argparse.Namespace) -> None:
+        r'''
         .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
         
-        :param parent: parent widget holding this widget
-        :type parent: PyQt6.QtWidgets.QWidget
-        :param str cmap: initial colormap used
+        :param args: argument namespace built with the argparse library. Contains all the arguments that the program may use
+        :type args: argparse.Namespace
         
-        :raises: 
-            * :python:`TypeError` if `not isinstance(cmap, str)`
+        :raises:
+            * :python:`TypeError` if :python:`not isinstance(args, argparse.Namespace)`
+            * :python:`IOError` if no files were provided
         '''
         
-        #: Parent widget
-        self.__parent = parent
+        super().__init__()
         
-        self.cmap     = cmap
-
-        # Set the figure and the axis
-        self.figure   = mplf.Figure()
-        self.__ax     = self.figure.add_subplot(111)
-        self.ax.set_axis_off()
+        if not isinstance(args, argparse.Namespace):
+            raise TypeError(f'args in Window instance has type {type(args)} but only argparse.Namespace is allowed.')
         
-        # Setup layout of the figure and axes
-        self.figure.subplots_adjust(left=0., bottom=0., right=1., top=1.)
-        self.ax.axes.tick_params(bottom=False, top=False, left=False, right=False)
+        #: Command line arguments
+        self.__args   = args
         
-        # Array containing the data. None means no data has been provided yet
-        self.__array  = None
+        ###########################
+        #        Load data        #
+        ###########################
         
-        # Artist that contains the image shown
-        self.__im_artist = self.ax.imshow(np.full((2, 2), 0), cmap='rainbow')
+        # Load 2D map
+        self.__image      = self.open_file(self.__args.image, self.__args.image_ext, ndim=2)  
+        
+        # Load data cube
+        self.__cube       = self.open_file(self.__args.cube, self.__args.cube_ext, ndim=3)  
+        
+        # Load cube model
+        self.__cube_model = self.open_file(self.__args.cube_model, self.__args.cube_model_ext, ndim=3)
+        
+        if all([i is None for i in (self.image, self.cube, self.cube_model)]):
+            raise IOError('At least one file must be provided. Either a 2D map with the -i (--image) tag, or the 3D data cube with -c (--cube) tag, or the 3D cube model with -m (--model) tag.')
+        
+        ##################################################################
+        #            Main frame that serves as central widget            #
+        ##################################################################
+        
+        self.mpl_im_widget = Mpl_im_canvas(self, 'rainbow')
+        
+        self.setCentralWidget(self.mpl_im_widget)
+        
+        ###############################
+        #            Docks            #
+        ###############################
+        
+        # We only show the dock with the spectrum if 2 or 3 files are provided
+        if self.cube is not None or self.cube_model is not None:
             
-        super().__init__(figure=self.figure)
-        
-        # Taken from Benoit but commented for now
-        '''
-        super().setSizePolicy(self, QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
-        '''
-        super().updateGeometry()
+            # Add a dock at the bottom that will hold the spectrum
+            self.bottom_dock = Dock_widget_spectrum(self)
             
+            self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, 
+                               self.bottom_dock, 
+                               Qt.Orientation.Horizontal
+                              )
+            
+            self.resizeDocks((self.bottom_dock, ), (300,), Qt.Orientation.Vertical)
+        
         return
     
     ###################################
@@ -74,161 +89,141 @@ class Mpl_im_canvas(FigureCanvas):
     ###################################
     
     @property
-    def ax(self):
+    def image(self) -> np.ndarray | None:
         r'''
         .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
         
-        Main axis associated to the figure.
-        '''
-    
-        return self.__ax
-    
-    @property
-    def array(self) -> np.ndarray:
-        r'''
-        .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
-        
-        Array that contains the data that are shown as an image.
+        2D map.
         '''
         
         return self.__image
-
-    @array.setter
-    def array(self, image: np.ndarray | None) -> None:
-        r'''
-        .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
-        
-        Update the array that contains the data associated to the image.
-        
-        :param image: image to show. If None, the image is not initialized.
-        :type image: numpy.ndarray
-    
-        :raises: 
-            * :python:`TypeError` if `not isinstance(image, np.ndarray)`
-            * :python:`ValueError` if `image.ndim < 2 or image.ndim > 3`
-            * :python:`NotImplementedError` if `image.ndim == 3`
-        '''
-        
-        # Checking for the data type of image
-        if isinstance(image, None):
-            self.__image = None
-            
-        elif not isinstance(image, np.ndarray):
-            raise TypeError(f'Trying to update the image with data of type {type(image)} but only np.ndarray is allowed.')
-        
-        # Checking for the dimensions of the image
-        if image.ndim < 2 or image.ndim > 3:
-            raise ValueError(f'Trying to update the image data of shape {image.shape} but data must have 2 or 3 dimensions to be shown.')
-            
-        elif image.ndim == 3:
-            raise NotImplementedError('Showing data cubes is not supported yet...')
-        
-        self.__image = image
-        
-        return
     
     @property
-    def cmap(self) -> str:
+    def cube(self) -> np.ndarray | None:
         r'''
         .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
         
-        Current colormap used to show the figure.
+        3D data cube.
         '''
         
-        return self.__cmap
+        return self.__cube
     
-    @cmap.setter
-    def cmap(self, cmap: str) -> None:
+    @property
+    def cube_model(self) -> np.ndarray | None:
         r'''
         .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
         
-        Set the cmap to a new one.
-        
-        :param str cmap: new colormap for the image
-       
-        :raises:
-            * :python:`TypeError` if `not isinstance(cmap, str)`
-            * :python:`ValueError` if the given cmap does not belong to the list of cmaps from matplotlib.pyplot
+        3D cube_model.
         '''
         
-        if not isinstance(cmap, str):
-            raise TypeError(f'cmap is of type {type(cmap)} but it must be of type str.')
-        
-        if cmap not in Mpl_im_canvas.__cmaps_ok:
-            raise ValueError(f'cmap is {cmap} which does not belong to the following list of cmaps from matplotlib: {Mpl_im_canvas.__cmaps_ok}.')
-        
-        self.__cmap   = cmap
-
-        return
+        return self.__cube_model
     
-    ##################################################
-    #        Updating properties of the image        #
-    ##################################################
+    #########################
+    #       IO methods      #
+    #########################
     
-    def update_image(self, image: np.ndarray) -> None:
+    @staticmethod
+    def open_file(file: str | None, ext: int,
+                  ndim : int | None = None
+                 ) -> np.ndarray:
         r'''
         .. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr>
         
-        :param image: image to show
-        :type image: numpy.ndarray
-        '''
+        Open a file.
         
-        # Update the array
-        self.array = image
-        
-        # If first instantiation of the artist
-        if self.__im_artist is None:
-            self.__im_artist = self.ax.imshow(self.array, cmap=self.cmap)
+        .. note::
             
-        # Otherwise, just update the data of the artist
+            Currently supported files are:
+                
+                * FITS
+                * FIT
+        
+        :param file: file to open. If None, no action is performed.
+        :type file: :python:`str` or :python:`None`
+        :param int ext: extension from which to get the file (e.g. for a FITS file). Not used if not applicable.
+        
+        Keyword arguments
+        -----------------
+        
+        :param int ndim: number of dimensions the data should have. Possibilities are 2 or 3. If None, no check is performed
+        
+        :returns: data from the file
+        :rtype: numpy.ndarray
+        
+        :raises: 
+            * :python:`TypeError` if :python:`not isinstance(file, str)` or :python:`not isinstance(ext, int)` or :python:`not isinstance(ndim, int)`
+            * :python:`IOError` if **file** is not a correct file name or symbolic link
+            * :python:`ValueError` if the file extension is not recognized or `ndim not in [2, 3]` or `data.ndim != ndim`
+        '''
+        
+        if file is None:
+            return None
+        
+        if ndim is not None and not isinstance(ndim, int):
+            raise TypeError(f'ndim has type {type(ndim)} but it should have type int.')
+        
+        if ndim is not None and ndim not in [2, 3]:
+            raise ValueError(f'ndim is equal to {ndim} but it must be 2 or 3.')
+        
+        if not isinstance(file, str):
+            raise TypeError(f'File has type {type(file)} but it must be of type str.')
+            
+        if not isinstance(ext, int):
+            raise TypeError(f'Extension number has type {type(ext)} but it must be of type int.')
+            
+        if not opath.isfile(file):
+            raise IOError(f'File {file} not found.')
+            
+        if (file_ext := opath.splitext(file)[1].lower()) in ['.fit', '.fits']:
+            
+            with fits.open(file) as hdul:    
+                data = hdul[ext].data
+        
         else:
-            self.im_artist.set_data(self.array)
-          
-        # Apply changes
-        self.updateGeometry()
-        
-        return
+            raise ValueError(f'File type with extension {file_ext} not supported. Supported types are: fits and fit.')
             
-class Mpl_image_widget(QWidget):
-    
-    def __init__(self, parent):
+        if ndim is not None and data.ndim != ndim:
+            raise ValueError(f'Data has dimensions {data.ndim} but it should have a dimension of {ndim}.')
+            
+        return data
         
-        self.parent = parent
-        
-        super().__init__(self, self.parent)
-        
-        # Setup custom canvas
-        self.canvas = Mpl_im_canvas()
-        
-        self.vbl = QVBoxLayout()
-        self.vbl.addWidget(self.canvas)
-        self.setLayout(self.vbl)
-        
-
-class Window(QMainWindow):
-    
-    def __init__(self, *arg, **kwargs):
-        
-        super().__init__(*arg, **kwargs)
-        
-        # Main frame that serves as central widget
-        #mpl_im_widget = Mpl_image_widget(self)
-        mpl_im_widget = Mpl_im_canvas(self, 'rainbow')
-        self.setCentralWidget(mpl_im_widget)
-        
-        # Add
-        
-        return
 
 def main(argv):
     
+    ##################################################
+    #             Command line arguments             #
+    ##################################################
+    
+    # Instantiate the argument parser
+    parser = argparse.ArgumentParser(prog        = 'EZQubeVis',
+                                     description = 'EZQubeVis is a light-weight cube and 2D map vizualization tool inspired by PyQubeVis.'
+                                    )
+    
+    # Mandatory arguments
+    parser.add_argument('-i', '--image',      type=str, nargs='?', help='2D map to open.')
+    parser.add_argument('-c', '--cube',       type=str, nargs='?', help='data cube to open')
+    parser.add_argument('-m', '--cube_model', type=str, nargs='?', help='cube model to open')
+    
+    parser.add_argument('-s', '--save',       type=str, nargs='?', help='File within which the cleaned map will be saved when the program terminates.')
+    
+    parser.add_argument('--image_ext',        type=int, nargs='?', default=0, help='File extension for the 2D map (if applicable).')
+    parser.add_argument('--cube_ext',         type=int, nargs='?', default=0, help='File extension for the 2D map (if applicable).')
+    parser.add_argument('--cube_model_ext',   type=int, nargs='?', default=0, help='File extension for the 2D map (if applicable).')
+    
+    # Get command-line arguments
+    args   = parser.parse_args()
+
+    #############################################
+    #           Setting up the window           #
+    #############################################
+    
     # Main application and window
-    app = QApplication(sys.argv)
+    app    = QApplication(sys.argv)
     
     # Handling SIGINT signal from terminal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     
-    main_window = Window()
+    main_window = Window(args)
     main_window.show()
     
     # Add the possibility to close the window through the terminal
